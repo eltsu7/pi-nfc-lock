048795508fc36b7bc313a63987a6d837c3f24cfb
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2022-08-20 17:55:23 +0300

    id validation from server

--- read.py
+++ read.py
@@ -1,15 +1,29 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import time
+from env import api_key, base_url
+import requests
 
 reader = SimpleMFRC522()
+
+def validate_id(id):
+    parameters = {
+        "api_key": api_key,
+        "key": id,
+    }
+
+    response = requests.get(base_url, params=parameters)
+    print(response.text)
 
 try:
     while True:
         id = reader.read_id()
         print(id)
-        # requests
+        validate_id(id)
         time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Goodbye!")
 
 finally:
     GPIO.cleanup()
